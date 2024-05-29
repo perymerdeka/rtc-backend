0a94d6988d280859465fa0511c86499ea7455657
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2024-05-29 12:43:22 +0700

    WIP: socket connected

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const port = 8000;
 
-
 const app = express();
+app.use(cors);
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
@@ -16,6 +17,7 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    console.log(socket.id)
     socket.on("disconnect", () => {
         console.log("User is disconnected");
 
