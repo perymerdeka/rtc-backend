08910e04abb5f8a03a53a370e9b28b25f716f1d4
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2024-06-08 23:09:29 +0700

    Update Controller

--- src/apps/room/controller.ts
+++ src/apps/room/controller.ts
@@ -1,7 +1,11 @@
 export const joinRoom = (): void => {
-    console.log("User is Joined");
-  };
-  
-  export const enterRoom = (): void => {
-    console.log("User is Entering Room");
-  };
+  console.log("User is Joined");
+};
+
+export const enterRoom = (): void => {
+  console.log("User is Entering Room");
+};
+
+export const createRoom = (): void => {
+  console.log("User is Creating Room");
+};
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const io = new Server(server, {
 });
 
 io.on('connect', (socket) => {
-    console.log('a socket connected');
+    console.log('a socket connected in the server', socket.id);
     
     // add room handler
     roomHandler(socket);
